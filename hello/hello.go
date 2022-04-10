7beca193ca57356f6deae813ec2ae82e85b2014d
Jacky Chan <find.jacky.chan@hotmail.com>
2022-04-10 18:07:35 +0800

    Call your code from another module

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"example.com/greetings"
+)
 
-import "rsc.io/quote"
+// import "rsc.io/quote"
 
 func main() {
-	fmt.Println(quote.Go())
+	// fmt.Println(quote.Go())
+
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
