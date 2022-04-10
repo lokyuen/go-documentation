b6db56410419aa107e02a807ff34c1c40ce6d4aa
Jacky Chan <find.jacky.chan@hotmail.com>
2022-04-10 18:35:48 +0800

    Return greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -15,12 +15,17 @@ func main() {
 	log.SetFlags(0)
 
 	// Get a greeting message and print it.
-	message, err := greetings.Hello("Gladys")
+	// message, err := greetings.Hello("Gladys")
+
+	// A slice of names
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
 
