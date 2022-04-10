0d5c153d4b5688c13ef9b226893da40c52517710
Jacky Chan <find.jacky.chan@hotmail.com>
2022-04-10 18:16:09 +0800

    Return and handle an error

--- hello/hello.go
+++ hello/hello.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"fmt"
 	"example.com/greetings"
 )
@@ -10,7 +11,15 @@ import (
 func main() {
 	// fmt.Println(quote.Go())
 
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
