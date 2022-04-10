cdaaadf0a50fd69a7fab437d0eabdc090246b964
Jacky Chan <find.jacky.chan@hotmail.com>
2022-04-10 20:16:28 +0800

    tutorial/create-a-module
    Squashed commit of the following:
    
    commit 1b4321645f5165833a2cb5fbe6c862f128dbf4b8
    Author: Jacky Chan <find.jacky.chan@hotmail.com>
    Date:   Sun Apr 10 20:15:39 2022 +0800
    
        Compile and install the application
    
    commit 1f6d235d8605bb44651fdad99ba245fb1525f98a
    Author: Jacky Chan <find.jacky.chan@hotmail.com>
    Date:   Sun Apr 10 18:54:50 2022 +0800
    
        Add a test
    
    commit b6db56410419aa107e02a807ff34c1c40ce6d4aa
    Author: Jacky Chan <find.jacky.chan@hotmail.com>
    Date:   Sun Apr 10 18:35:48 2022 +0800
    
        Return greetings for multiple people
    
    commit c02921bc46169f9c6ea728e6dc7bd2889de1527b
    Author: Jacky Chan <find.jacky.chan@hotmail.com>
    Date:   Sun Apr 10 18:26:37 2022 +0800
    
        Return a random greeting
    
    commit 0d5c153d4b5688c13ef9b226893da40c52517710
    Author: Jacky Chan <find.jacky.chan@hotmail.com>
    Date:   Sun Apr 10 18:16:09 2022 +0800
    
        Return and handle an error
    
    commit 7beca193ca57356f6deae813ec2ae82e85b2014d
    Author: Jacky Chan <find.jacky.chan@hotmail.com>
    Date:   Sun Apr 10 18:07:35 2022 +0800
    
        Call your code from another module
    
    commit 82d0a7d3a0c9d6545bbca36067016c01428f0b78
    Author: Jacky Chan <find.jacky.chan@hotmail.com>
    Date:   Sun Apr 10 18:00:27 2022 +0800
    
        Start a module that others can use

---
+++ greetings/greetings.go
@@ -0,0 +1,52 @@
+package greetings
+
+import (
+	"time"
+	"math/rand"
+	"errors"
+	"fmt"
+)
+
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+// Hellos returns a map that associates each of the named people with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	messages := make(map[string]string)
+	// Loop through the received slice of names, calling the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
+}
+
+
--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"fmt"
+	"example.com/greetings"
+)
 
-import "rsc.io/quote"
+// import "rsc.io/quote"
 
 func main() {
-	fmt.Println(quote.Go())
+	// fmt.Println(quote.Go())
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Get a greeting message and print it.
+	// message, err := greetings.Hello("Gladys")
+
+	// A slice of names
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(messages)
 }
+
