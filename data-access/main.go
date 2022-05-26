225defaea9525f68be5c51b74dd05c0032fb4f52
Jacky <find.jacky.chan@hotmail.com>
2022-05-26 16:34:38 +0800

    Add data

--- data-access/main.go
+++ data-access/main.go
@@ -57,6 +57,19 @@ func albumByID(id int64) (Album, error) {
 	return alb, nil
 }
 
+func addAlbum(alb Album) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+
+	return id, nil
+}
+
 func main() {
 	// Capture connection properties.
 	cfg := mysql.Config{
@@ -95,4 +108,14 @@ func main() {
 
 	fmt.Printf("Album found: %v\n", alb)
 
+	albID, err := addAlbum(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("ID of added album: %v\n", albID)
 }
