88ddad9ace4e84d6d48393b3912b78f068c2a638
Jacky <find.jacky.chan@hotmail.com>
2022-05-26 16:09:44 +0800

    Query for a single row

--- data-access/main.go
+++ data-access/main.go
@@ -43,6 +43,20 @@ func albumsByArtist(name string) ([]Album, error) {
 	return albums, nil
 }
 
+func albumByID(id int64) (Album, error) {
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album where id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsByID %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+
+	return alb, nil
+}
+
 func main() {
 	// Capture connection properties.
 	cfg := mysql.Config{
@@ -73,5 +87,12 @@ func main() {
 	}
 
 	fmt.Printf("Albums found: %v\n", albums)
+
+	alb, err := albumByID(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Album found: %v\n", alb)
 
 }
